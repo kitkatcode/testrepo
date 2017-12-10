42eec9f362fc5859a8872ddd4ce2d2a797ff2232
kitkatcode <lafar2@gmail.com>
2017-12-10 17:51:08 +0100

    Adding first test to repo.
    It was created test for numbers.

---
+++ myFirstTest/app/src/main/java/com/example/mic/myapplication/TestExample.java
@@ -0,0 +1,12 @@
+package com.example.mic.myapplication;
+
+/**
+ * Created by mic on 09/12/2017.
+ */
+
+public class TestExample {
+
+    public int addNumber(int a, int b){
+        return a+b;
+    }
+}
---
+++ myFirstTest/app/src/test/java/com/example/mic/myapplication/ExampleUnitTest.java
@@ -0,0 +1,28 @@
+package com.example.mic.myapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+
+    @Test
+    public void test(){
+        /*This is my firt Test in JUnit*/
+        int a = 5;
+        int b = 12;
+        TestExample testExample = new TestExample();
+        int result = testExample.addNumber(a,b);
+
+        assertEquals("Wynik testu:",17,result);
+    }
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
